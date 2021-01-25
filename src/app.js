279c501f045073789b0b33f112fed3b2ce691607
Daniele Cencig <daniele.cencig@webformat.com>
2021-01-25 16:00:05 +0100

    Add index.html and routing

--- src/app.js
+++ src/app.js
@@ -1,3 +1,5 @@
+import path from "path";
+import { fileURLToPath } from "url";
 import dotenv from "dotenv";
 import express from "express";
 import helmet from "helmet";
@@ -8,5 +10,11 @@ const app = express();
 
 app.use(helmet());
 app.use(express.json());
+
+const currPath = path.dirname(fileURLToPath(import.meta.url));
+
+app.get("/", (req, res) => {
+    res.sendFile(path.resolve(currPath, "..", "static", "index.html"));
+});
 
 export default app;
