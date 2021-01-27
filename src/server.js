0b33947b9ba5552b9f7c0e257dd6eede8f7566ac
Daniele Cencig <daniele.cencig@webformat.com>
2021-01-27 12:32:45 +0100

    create node server

--- src/server.js
+++ src/server.js
@@ -2,12 +2,14 @@
 
 require("dotenv").config();
 
+const http = require("http");
+
 const app = require("./app");
 const Logger = require("./lib/logger.js");
 
 const appPort = process.env.PORT || "9000";
 
-app.listen(appPort, "0.0.0.0", () => {
+http.createServer(app).listen(appPort, "0.0.0.0", () => {
     Logger.info(`listening on port ${appPort}`);
     Logger.info(`Environment: ${process.env.NODE_ENV}`);
 });
