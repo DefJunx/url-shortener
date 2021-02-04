b0bf34dd0d175bace03428b31c511e6233c5e693
Daniele Cencig <daniele.cencig@webformat.com>
2021-02-04 17:23:07 +0100

    Fix config

--- src/db/db.js
+++ src/db/db.js
@@ -3,4 +3,4 @@ const Logger = require("../lib/logger");
 
 Logger.debug(`process.env.DB_URL: ${process.env.DB_URL}`);
 
-module.exports = monk(process.env.DB_URL);
+module.exports = monk(process.env.DB_URL, { authSource: "admin" });
